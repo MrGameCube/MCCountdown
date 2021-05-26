543927a14d943d35226a9e32e88b524a4641724d
Matteo Mohr <mrgamecube@muffinanddonut.de>
2021-05-26 17:01:36 +0200

    Open and Closed State

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-
+let open = false;
 const date = new Date();
-date.setSeconds(0);
-date.setMinutes(0);
-date.setHours(18);
+const OPEN_HOUR_UTC = 16;
+const CLOSE_HOUR_UTC = 2;
+const curHours = new Date().getUTCHours();
+if(curHours >= OPEN_HOUR_UTC || curHours <= CLOSE_HOUR_UTC) {
+    open = true;
+    if(curHours >= OPEN_HOUR_UTC) {
+        date.setUTCDate(date.getUTCDate()+1);
+    }
+    date.setUTCHours(CLOSE_HOUR_UTC);
+} else {
+    date.setSeconds(0);
+    date.setMinutes(0);
+    date.setUTCHours(OPEN_HOUR_UTC);
+}
+
+const closedMessage = "Minecraft Server wieder aus in:";
+const openMessage = "Minecraft Server wieder offen in:";
+
 
 document.querySelector('#app').innerHTML = `
-<h1>Minecraft Server wieder offen in:</h1>
+<h1>${open ? closedMessage : openMessage}</h1>
 <countdown-time autostart datetime="${date.getTime()}"></countdown-time>
 `
